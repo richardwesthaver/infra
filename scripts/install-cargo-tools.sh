7a03ca266a5d60a7d558babb91ee0fccb22bbf46
ellis <ellis@rwest.io>
2024-01-03 12:53:45 -0500

    etc

--- scripts/install-cargo-tools.sh
+++ scripts/install-cargo-tools.sh
@@ -3,6 +3,7 @@ cargo install cargo-add --root /usr/local/
 cargo install cbindgen --root /usr/local/
 cargo install cargo-asm --root /usr/local/
 # cargo install t-rec # requires -lX11
+cargo install bottom --root /usr/local/
 cargo install trunk --root /usr/local/
 cargo install tokio-console --root /usr/local/
 # cargo install sqlx-cli
--- virt/build-ci-worker.sh
+++ virt/build-ci-worker.sh
@@ -6,15 +6,15 @@
 id=$(buildah from alpine-base)
 buildah add $id etc/skel /etc/skel
 buildah run $id adduser worker -D
-buildah run $id apk add build-base zstd-dev sbcl curl make git linux-headers cargo openssl perl llvm
+buildah run $id apk add build-base zstd-dev sbcl curl make git linux-headers cargo openssl perl llvm clang
 buildah run $id mkdir /store
 buildah run $id mkdir /stash
 buildah config --volume /store $id
 buildah run --net host $id hg clone https://vc.compiler.company/comp/infra
-buildah config --workingdir  $id infra
-buildah run --net host $id sh -c 'make worker'
+buildah config --workingdir /infra $id 
+buildah run --net host $id sh -c 'make worker -j4'
 buildah run --net host $id sh -c 'scripts/install-cargo-tools.sh'
 buildah run --net host $id sh -c 'make clean'
-buildah config --entrypoint '["/usr/local/bin/sbcl"]' $id
+buildah config --healthcheck bottom
 buildah config --workingdir /stash
 buildah commit $id ci-worker
