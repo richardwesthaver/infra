b44329c1f0227beca32a37a871788b176e708fdf
ellis <ellis@rwest.io>
2024-01-03 14:56:40 -0500

    testing

--- virt/build-ci-worker.sh
+++ virt/build-ci-worker.sh
@@ -5,16 +5,18 @@
 
 id=$(buildah from alpine-base)
 buildah add $id etc/skel/ /etc/skel/
-buildah add $id etc/sbclrc /etc/sbclrc
 buildah run $id adduser worker -D
 buildah run $id apk add build-base zstd-dev sbcl curl make git linux-headers cargo openssl perl llvm clang
 buildah run $id mkdir /store
 buildah run $id mkdir /stash
+buildah run $id mkdir /usr/share/lisp
+buildah run $id mkdir /usr/local/share/lisp
 buildah config --volume /store $id
 buildah run --net host $id hg clone https://vc.compiler.company/comp/infra
 buildah config --workingdir /infra $id 
 buildah run --net host $id sh -c 'make worker -j4'
 buildah run --net host $id sh -c 'scripts/install-cargo-tools.sh'
 buildah run --net host $id sh -c 'make clean'
+buildah add $id etc/sbclrc /etc/sbclrc # add this AFTER building sbcl
 buildah config --workingdir /stash $id
 buildah commit $id ci-worker
