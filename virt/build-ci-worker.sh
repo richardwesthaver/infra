da14fd338ad68274924f8d350ed382b0de5a0c20
ellis <ellis@rwest.io>
2024-01-03 13:23:24 -0500

    worker config

--- virt/build-ci-worker.sh
+++ virt/build-ci-worker.sh
@@ -4,7 +4,8 @@
 # dedicated pod named 'ci'.
 
 id=$(buildah from alpine-base)
-buildah add $id etc/skel /etc/skel
+buildah add $id etc/skel/ /etc/skel/
+buildah add $id etc/sbclrc /etc/sbclrc
 buildah run $id adduser worker -D
 buildah run $id apk add build-base zstd-dev sbcl curl make git linux-headers cargo openssl perl llvm clang
 buildah run $id mkdir /store
@@ -15,6 +16,6 @@ buildah config --workingdir /infra $id
 buildah run --net host $id sh -c 'make worker -j4'
 buildah run --net host $id sh -c 'scripts/install-cargo-tools.sh'
 buildah run --net host $id sh -c 'make clean'
-buildah config --healthcheck bottom
-buildah config --workingdir /stash
+buildah config --healthcheck btm $id
+buildah config --workingdir /stash $id
 buildah commit $id ci-worker
