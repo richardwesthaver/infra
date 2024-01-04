ca74f267c8ce68e21d7c06c0ab279af39fd2a8ae
ellis <ellis@rwest.io>
2024-01-04 00:43:47 -0500

    ugh bash

--- virt/build-ci-operator.sh
+++ virt/build-ci-operator.sh
@@ -4,7 +4,7 @@ buildah run $id adduser $USER -D
 buildah config --volume /mnt/y $id
 # save a fresh core
 buildah config --workingdir /infra $id
-buildah run --net host $id apk add --no-cache tree-sitter-dev autoconf texinfo libgccjit-dev zlib-dev gnutls-dev sysprof gpg
+buildah run --net host $id apk add --no-cache tree-sitter-dev autoconf texinfo libgccjit-dev zlib-dev gnutls-dev sysprof gpg bash
 buildah run --net host $id hg pull -u
 buildah run --net host $id sh -c 'cd /usr/local/share/lisp/core && hg pull -u'
 buildah run --net host $id make operator
