172f1470256db24a36b203c15918f69054e5c9a7
Richard Westhaver <ellis@rwest.io>
2024-01-16 22:02:03 -0500

    box updates

--- scripts/install-rocksdb-pack.sh
+++ scripts/install-rocksdb-pack.sh
@@ -2,7 +2,8 @@
 set -e
 cd build/src
 curl -O https://packy.compiler.company/dist/x86_64-unknown-linux-gnu/rocksdb-binary.tar.zst
-unzstd rocksdb-binary.tar.zst | tar xvf -
+unzstd rocksdb-binary.tar.zst
+tar -xvf rocksdb-binary.tar
 cd rocksdb
 cp librocksdb.* /usr/local/lib/
 cp -rf include/* /usr/local/include/
