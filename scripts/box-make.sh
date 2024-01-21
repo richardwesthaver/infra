8b87643f88a3699f973dbd114381407128b2513d
Richard Westhaver <ellis@rwest.io>
2024-01-21 01:10:36 -0500

    .

--- scripts/box-make.sh
+++ scripts/box-make.sh
@@ -3,7 +3,7 @@ set -e
 img="${1:-infra/box}"
 rule="${2:-sbcl-build}"
 # in nushell
-cmd="cd /usr/local/src/infra; hg pull -u; make clean $rule"
-podman run --name "$rule" --replace -it "$img" -e "$cmd"
+cmd="cd /usr/local/src/infra && hg pull -u && make clean $rule"
+podman run --name "$rule" --replace -it "$img" "$cmd"
 podman cp --overwrite $rule:/usr/local/src/infra/dist .
 podman cp --overwrite $rule:/usr/local/src/infra/build .
