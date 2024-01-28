704e200a197d278a409a2cdbf303b49afeb116b1
Richard Westhaver <ellis@rwest.io>
2024-01-28 13:08:04 -0500

    try registering with ql local projects

--- scripts/sbcl-make-bin.sh
+++ scripts/sbcl-make-bin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 CORE_SRC=${2:-/usr/local/src/core/}
 FORM="(progn (pushnew #P\"$CORE_SRC\" asdf:*central-registry*) "
-FORM+="(ql:quickload :std) "
+FORM+="(pushnew #P\"$CORE_SRC\" ql:*local-project-directories*) (ql:quickload :std)"
 FORM+=" (ql:quickload \""
 FORM+="${1:-bin/skel}"
 FORM+="\") (asdf:make \""
--- scripts/sbcl-save-core.sh
+++ scripts/sbcl-save-core.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # save an sbcl core image
 CORE_SRC=${2:-/usr/local/src/core/}
-FORM="(progn (pushnew #P\"$CORE_SRC\" asdf:*central-registry*) (ql:quickload :std) ${3} (save-lisp-and-die \"${1:-std.core}\"))"
+FORM="(progn (pushnew #P\"$CORE_SRC\" asdf:*central-registry*) (pushnew #P\"$CORE_SRC\" ql:*local-project-directories*) (ql:quickload :std) ${3} (save-lisp-and-die \"${1:-std.core}\"))"
 sbcl --noinform --non-interactive --eval "$FORM"
