c6db6dc0a949a34c520a32017dcbbdf684fe4fae
Richard Westhaver <ellis@rwest.io>
2024-01-28 11:55:44 -0500

    asdf needs trailing slash

--- scripts/sbcl-save-core.sh
+++ scripts/sbcl-save-core.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # save an sbcl core image
-CORE_SRC=${2:-/usr/local/src/core}
+CORE_SRC=${2:-/usr/local/src/core/}
 FORM="(progn (pushnew #P\"$CORE_SRC\" asdf:*central-registry*) (ql:quickload :std) ${3} (save-lisp-and-die \"${1:-std.core}\"))"
 sbcl --noinform --non-interactive --eval "$FORM"
