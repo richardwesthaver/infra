668bba1082864b718315e4d9613df612b551bf1d
Richard Westhaver <ellis@rwest.io>
2024-01-31 16:51:44 -0500

    fixed box-make

--- scripts/box-make.sh
+++ scripts/box-make.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+
+# SHELL=nu
 set -e
 img="${1:-infra/box}"
 rule="${2:-sbcl-build}"
 # in nushell
-cmd="cd /usr/local/src/infra && hg pull -u && make build clean dist $rule"
-podman run --name "$rule" --replace -it "$img" sh -c "$cmd"
-podman cp --overwrite $rule:/usr/local/src/infra/dist .
-podman cp --overwrite $rule:/usr/local/src/infra/build .
+cmd="cd infra; hg pull -u; make build clean dist $rule"
+podman run --name "$rule" --replace -it "$img" -c "$cmd"
+podman cp --overwrite $rule:infra/dist .
+podman cp --overwrite $rule:infra/build .
