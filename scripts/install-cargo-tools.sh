4cdef5c029079252952aaa3a4b013bb25d814bdd
Richard Westhaver <ellis@rwest.io>
2024-02-01 16:03:17 -0500

    add zellij to cargo tools

--- scripts/install-cargo-tools.sh
+++ scripts/install-cargo-tools.sh
@@ -2,11 +2,12 @@
 cargo install cargo-add --root /usr/local/
 cargo install cbindgen --root /usr/local/
 cargo install cargo-asm --root /usr/local/
-# cargo install t-rec # requires -lX11
 cargo install bottom --root /usr/local/
-cargo install trunk --root /usr/local/
-# cargo install tokio-console --root /usr/local/
+cargo install tokio-console --root /usr/local/
+cargo install zellij --root /usr/local/
+# cargo install t-rec # requires -lX11
+#cargo install trunk --root /usr/local/
 # cargo install cross --git https://github.com/cross-rs/cross --root /usr/local/
 # cargo install sqlx-cli
-cargo install wasm-bindgen-cli --root /usr/local/
-cargo install wasm-opt --root /usr/local/
+# cargo install wasm-bindgen-cli --root /usr/local/
+# cargo install wasm-opt --root /usr/local/
