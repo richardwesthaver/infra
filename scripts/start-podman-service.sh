a23489cb7c753984f35838350a0d110e5b9eba64
Richard Westhaver <ellis@rwest.io>
2024-02-03 20:58:05 -0500

    bump

--- scripts/start-podman-service.sh
+++ scripts/start-podman-service.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # tcp://localhost:4282
-podman system service --time=0 unix:///run/user/$UID/podman.sock # --cors
+podman system service --time=0 unix:///run/user/$UID/podman.sock
