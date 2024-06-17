2ebd90385274ea3a6e199db6deaff077651312da
Richard Westhaver <ellis@rwest.io>
2024-06-17 02:23:39 +0000

    fix bundle

--- scripts/bundle-packy.sh
+++ scripts/bundle-packy.sh
@@ -34,5 +34,5 @@ for i in $(find . -name ".hg" | cut -c 3-); do
 done
 
 # archive all *.git bundles and Mercurial .hg bundle
-cd $WD/src && tar -I 'zstd' -cf $OUT/$BUNDLE.tar.zst packy/*
+cd $WD/src && tar -I 'zstd' -cf $OUT/$BUNDLE.tar.zst packy
 echo "Done."
