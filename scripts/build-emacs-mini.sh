87041be50f18d977ef3c526ac317798beb17f940
Richard Westhaver <ellis@rwest.io>
2024-07-07 20:53:05 -0400

    slight tweaks to emacs build script

--- scripts/build-emacs-mini.sh
+++ scripts/build-emacs-mini.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 CPUS=$(getconf _NPROCESSORS_ONLN)
 TARGETDIR="${1:-.stash/src/emacs}"
-cd $TARGETDIR
+cd $TARGETDIR && \
 ./configure --without-all --with-x-toolkit=no --without-x --enable-link-time-optimization \
             --with-json=ifavailable --with-gif=ifavailable --with-modules --with-gnutls=ifavailable \
-            --prefix=/usr/local
+            --prefix=/usr/local && \
 NATIVE_FULL_AOT=1 make -j$CPUS
