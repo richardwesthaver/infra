416f86fe1571a13f65963b86863cb244a348ddfe
Richard Westhaver <ellis@rwest.io>
2024-07-08 12:49:42 -0400

    fix core pack install

--- scripts/install-core-pack.sh
+++ scripts/install-core-pack.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
-cd .stash/src
-curl -O https://packy.compiler.company/dist/x86_64-unknown-linux-gnu/core.tar.zst
+TARGET="${1:-x86_64-unknown-linux-gnu}"
+cd .stash/tmp
+curl -O "https://packy.compiler.company/dist/${TARGET}/pack/core.tar.zst"
 unzstd core.tar.zst
 tar -xvf core.tar
 cd core
