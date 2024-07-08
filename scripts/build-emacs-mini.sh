cfa7ee0425950c07bd3b5add56f33ad4f06d0093
Richard Westhaver <ellis@rwest.io>
2024-07-07 20:34:21 -0400

    allow emacs-mini to be built on alpine

--- scripts/build-emacs-mini.sh
+++ scripts/build-emacs-mini.sh
@@ -1,12 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/sh
 CPUS=$(getconf _NPROCESSORS_ONLN)
 TARGETDIR="${1:-.stash/src/emacs}"
-CONFIG=(--without-all
-        --with-x-toolkit=no
-        --without-x
-	--enable-link-time-optimization
-	--with-json=ifavailable
-        --with-gif=ifavailable
-	--with-modules
-        --prefix=/usr/local)
-cd $TARGETDIR &&./configure ${CONFIG[@]} && NATIVE_FULL_AOT=1 make -j$CPUS
+cd $TARGETDIR
+./configure --without-all --with-x-toolkit=no --without-x --enable-link-time-optimization \
+            --with-json=ifavailable --with-gif=ifavailable --with-modules --prefix=/usr/local
+NATIVE_FULL_AOT=1 make -j$CPUS
