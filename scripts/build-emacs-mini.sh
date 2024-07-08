b67fc0936b6b1bd03f36e988d4e8d848e008d751
Richard Westhaver <ellis@rwest.io>
2024-07-07 20:54:01 -0400

    gnutls ifavailable

--- scripts/build-emacs-mini.sh
+++ scripts/build-emacs-mini.sh
@@ -3,5 +3,6 @@ CPUS=$(getconf _NPROCESSORS_ONLN)
 TARGETDIR="${1:-.stash/src/emacs}"
 cd $TARGETDIR
 ./configure --without-all --with-x-toolkit=no --without-x --enable-link-time-optimization \
-            --with-json=ifavailable --with-gif=ifavailable --with-modules --prefix=/usr/local
+            --with-json=ifavailable --with-gif=ifavailable --with-modules --with-gnutls=ifavailable \
+            --prefix=/usr/local
 NATIVE_FULL_AOT=1 make -j$CPUS
