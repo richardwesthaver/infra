f84f820165daffe304e5cd0b26606f2aeb657159
Richard Westhaver <ellis@rwest.io>
2024-07-08 07:57:23 -0400

    sbcl build script

---
+++ scripts/build-sbcl.sh
@@ -0,0 +1,4 @@
+./scripts/get-sbcl.sh
+cd .stash/src/sbcl
+echo \""2.4.6:$(git rev-parse --short HEAD)"\" > version.lisp-expr &&
+  ./make.sh --dynamic-space-size=8Gb --without-gencgc --with-mark-region-gc --fancy
