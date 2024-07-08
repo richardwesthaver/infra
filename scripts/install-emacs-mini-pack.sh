7e9802730aa37be2ffe4efbb3d248e8e7512cd5d
Richard Westhaver <ellis@rwest.io>
2024-07-07 21:59:36 -0400

    ugh3

--- scripts/install-emacs-mini-pack.sh
+++ scripts/install-emacs-mini-pack.sh
@@ -5,5 +5,5 @@ TARGET="${1:-x86_64-unknown-linux-gnu}"
 curl -O "https://packy.compiler.company/dist/${TARGET}/pack/emacs-mini.tar.zst"
 unzstd emacs-mini.tar.zst
 tar -xvf emacs-mini.tar
-cd emacs
+cd emacs*/
 make install
