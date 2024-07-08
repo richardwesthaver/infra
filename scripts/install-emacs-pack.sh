b9e1fa0ace85cd558588f67c481f78505956c610
Richard Westhaver <ellis@rwest.io>
2024-07-08 13:16:15 -0400

    fix

--- scripts/install-emacs-pack.sh
+++ scripts/install-emacs-pack.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
+TARGET="${1:-x86_64-unknown-linux-gnu}"
 cd .stash/src
-curl -O https://packy.compiler.company/dist/x86_64-unknown-linux-gnu/emacs.tar.zst
+curl -O "https://packy.compiler.company/dist/${TARGET}/pack/emacs.tar.zst"
 unzstd emacs.tar.zst
 tar -xvf emacs.tar
 cd emacs
