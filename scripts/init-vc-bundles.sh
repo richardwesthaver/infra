9fc0ec4d193f40afc32899035f5bb91c3d12c9e9
Richard Westhaver <ellis@rwest.io>
2024-07-21 18:15:38 -0400

    init-vc-bundles

---
+++ scripts/init-vc-bundles.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# given a directory bundle files (.hg), unbundle and replace
+# the files with bare directories.
+
+DIR="${1:-/home/vc/src/}"
+cd $DIR
+for i in $(find . -type f -name ".hg"); do
+  echo "unbundling $i"
+  r=(basename "$i" .hg)
+  hg init "$r"
+  cd $r && hg unbundle $DIR/$i && cd $DIR
+done
+            
