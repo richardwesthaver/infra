69f104be0694a353f599886b2dcd7fa424efe56f
Richard Westhaver <ellis@rwest.io>
2024-07-21 21:58:15 -0400

    fix

--- scripts/install-emacs-mini-pack.sh
+++ scripts/install-emacs-mini-pack.sh
@@ -2,9 +2,9 @@
 set -e
 cd .stash/tmp
 TARGET="${1:-x86_64-unknown-linux-gnu}"
-curl -O "https://packy.compiler.company/dist/${TARGET}/pack/emacs-mini.tar.zst"
-unzstd emacs-mini.tar.zst
-tar -xvf emacs-mini.tar
+curl -O "https://packy.compiler.company/dist/${TARGET}/pack/emacs-mini-binary.tar.zst"
+unzstd emacs-mini-binary.tar.zst
+tar -xvf emacs-mini-binary.tar
 cd emacs*/
 ./configure --without-all --with-x-toolkit=no --without-x --enable-link-time-optimization \
             --with-json=ifavailable --with-gif=ifavailable --with-modules --with-gnutls=ifavailable \
