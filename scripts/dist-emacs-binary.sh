6cb851c1a06962a205df1d87197a961927d9365f
Richard Westhaver <ellis@rwest.io>
2024-07-30 19:00:43 -0400

    fixes

--- scripts/dist-emacs-binary.sh
+++ scripts/dist-emacs-binary.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-set -e
 name="${1:-emacs-31.0.50}"
 cd .stash/src/emacs
 ./make-dist --tar --no-compress
-zstd -22 "${name}.tar" -o "../../emacs.tar.zst"
+zstd -8 "${name}.tar" -o "../../emacs.tar.zst"
--- scripts/install-emacs-mini-pack.sh
+++ scripts/install-emacs-mini-pack.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 set -e
-cd .stash/tmp
 TARGET="${1:-x86_64-unknown-linux-gnu}"
-curl -O "https://packy.compiler.company/dist/${TARGET}/pack/emacs-mini-binary.tar.zst"
-unzstd emacs-mini-binary.tar.zst
-tar -xvf emacs-mini-binary.tar
-cd emacs*/
-./configure --without-all --with-x-toolkit=no --without-x --enable-link-time-optimization \
-            --with-json=ifavailable --with-gif=ifavailable --with-modules --with-gnutls=ifavailable \
-            --with-zlib --with-native-compilation --prefix=/usr/local
+cd .stash/tmp
+curl -O "https://packy.compiler.company/dist/${TARGET}/pack/emacs-mini.tar.zst"
+unzstd emacs-mini.tar.zst
+tar -xvf emacs-mini.tar
+cd emacs-mini
 make install
