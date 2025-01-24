78238fcc738086ed97594b07a08739ecf7a1efca
Richard Westhaver <ellis@rwest.io>
2025-01-24 16:07:39 -0500

    bump

--- scripts/get-blake3.sh
+++ scripts/get-blake3.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # get blake3 source code
-git clone https://vc.compiler.company/packy/blake3 .stash/src/blake3
+git clone https://github.com/BLAKE3-team/BLAKE3 .stash/src/blake3 # https://vc.compiler.company/packy/blake3 
--- scripts/get-rocksdb.sh
+++ scripts/get-rocksdb.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # get RocksDB source code
-git clone https://vc.compiler.company/packy/rocksdb .stash/src/rocksdb
+git clone https://github.com/facebook/rocksdb .stash/src/rocksdb # https://vc.compiler.company/packy/rocksdb
