7b7c3489cee2d306e126eb4424f0e853834cdc5c
Richard Westhaver <ellis@rwest.io>
2025-01-29 18:07:26 -0500

    refactoring

--- scripts/install-emacs-mini-pack.sh
+++ scripts/install-emacs-mini-pack.sh
@@ -2,9 +2,15 @@
 set -e
 TARGET="${1:-x86_64-unknown-linux-gnu}"
 VERSION="${2:-31.0.50}"
+pack="${PACKY_URL}/dist/${TARGET}/emacs-mini.tar.zst"
 cd .stash/tmp
-curl -O "https://packy.compiler.company/dist/${TARGET}/pack/emacs-mini.tar.zst"
-unzstd emacs-mini.tar.zst
-tar -xvf emacs-mini.tar
+if [ ! -d "core" ]; then
+  case $PACKY_URL in
+    "http"*) curl -O $pack ;;
+    *) cp $pack ./ ;;
+  esac
+  unzstd emacs-mini.tar.zst
+  tar -xvf emacs-mini.tar
+fi
 cd "emacs-${VERSION}"
 make install
---
+++ scripts/install-tree-sitter.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+TARGETDIR="${1:-.stash/src/tree-sitter}"
+PREFIX=$(realpath "${2:-${PREFIX:-/usr/local}}")
+cp $TARGETDIR/libtree-sitter.so $PREFIX/lib/
+cp -rf $TARGETDIR/lib/include/* $PREFIX/include/
