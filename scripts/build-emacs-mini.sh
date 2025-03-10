8ad0bbca87b9bfbe88691052e1e8760a062e0bdb
Richard Westhaver <ellis@rwest.io>
2025-03-10 19:31:38 -0400

    new gc for emacs

--- scripts/build-emacs-mini.sh
+++ scripts/build-emacs-mini.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+set -e
 CPUS=$(getconf _NPROCESSORS_ONLN)
 TARGETDIR="${1:-.stash/src/emacs}"
-cd $TARGETDIR && ./autogen.sh && \
+cd "$TARGETDIR" && ./autogen.sh && \
 ./configure --without-all --with-x-toolkit=no --without-x --enable-link-time-optimization \
-            --with-json=ifavailable --with-gif=ifavailable --with-modules --with-gnutls=ifavailable \
-            --with-zlib --with-native-compilation --prefix=/usr/local && \
-NATIVE_FULL_AOT=1 make -j$CPUS
+            --with-gif=ifavailable --with-modules --with-gnutls=ifavailable \
+            --with-zlib --prefix=/usr/local \
+	    --disable-gc-mark-trace
+NATIVE_FULL_AOT=1 make -j"$CPUS"
--- scripts/build-emacs.sh
+++ scripts/build-emacs.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
+set -e
 CPUS=$(getconf _NPROCESSORS_ONLN)
 TARGETDIR="${1:-.stash/src/emacs}"
 CONFIG=(--with-mailutils
 	--with-imagemagick
-	--with-x-toolkit=gtk
 	--without-pop
         --with-tree-sitter
 	--without-sound
-	--with-json
 	--enable-link-time-optimization
-	--with-modules)
+	--with-modules
+	--disable-gc-mark-trace)
 cd $TARGETDIR
 ./configure ${CONFIG[@]} 
-NATIVE_FULL_AOT=1 make -j$CPUS
+NATIVE_FULL_AOT=1 make -j"$CPUS"
