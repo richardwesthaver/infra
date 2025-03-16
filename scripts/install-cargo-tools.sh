43a42b6ae650b02033ec6a2ddbc8287c78b14369
Richard Westhaver <ellis@rwest.io>
2025-03-15 20:48:57 -0400

    fixes

--- scripts/install-cargo-tools.sh
+++ scripts/install-cargo-tools.sh
@@ -15,4 +15,4 @@ cargo install cross --git https://github.com/cross-rs/cross --root /usr/local/
 cargo install wasm-bindgen-cli --root /usr/local/
 cargo install wasm-opt --root /usr/local/
 cargo install wasm-pack --root /usr/local/
-cargo install cargo-c --rot /usr/local/
+cargo install cargo-c --root /usr/local/
