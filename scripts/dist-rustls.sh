1b9d9ca1bf582896f8d78e4d3c60df1e7ba94e46
Richard Westhaver <ellis@rwest.io>
2025-06-07 16:23:15 -0400

    remove old bootstrap method

--- scripts/dist-rustls.sh
+++ scripts/dist-rustls.sh
@@ -4,4 +4,3 @@ mkdir rustls
 cp src/rustls-ffi/librustls.so src/rustls-ffi/librustls/src/rustls.h rustls/
 tar -I 'zstd' -cf rustls.tar.zst rustls
 rm -rf rustls
-
--- scripts/install-tree-sitter.sh
+++ scripts/install-tree-sitter.sh
@@ -3,4 +3,4 @@ set -e
 TARGETDIR="${1:-.stash/src/tree-sitter}"
 PREFIX=$(realpath "${2:-${PREFIX:-/usr/local}}")
 cp "$TARGETDIR/libtree-sitter.so" "$PREFIX/lib/"
-cp -rf "$TARGETDIR/lib/include/*" "$PREFIX/include/"
+cp -rf "$TARGETDIR/lib/include/." "$PREFIX/include/"
