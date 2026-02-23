6cdf786172b316c28281dc834916e810474718a1
Richard Westhaver <ellis@rwest.io>
2026-02-23 17:24:31 -0500

    bump

--- scripts/install-core-pack.sh
+++ scripts/install-core-pack.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 set -ex
-mkdir -pv .stash/cache/tmp
-TARGET="${1:-x86_64-unknown-linux-gnu}"
 cd .stash/cache/tmp
-pack="${PACKY_HOME}/dist/${TARGET}/core.zst"
-if [ ! -d "core" ]; then
-  case $PACKY_URL in
-    "http"*) curl -O $pack ;;
-    *) cp $pack ./ ;;
-  esac
-  unzstd core.zst
-fi
-cd core
+unzstd core.zst
 # mv bin/* ${PREFIX}/bin/
 mv core ${PREFIX}/bin/
 chmod +x ${PREFIX}/bin/core
